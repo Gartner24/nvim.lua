9a213f965da1bd4d0a82ff531a8c3f9ad69b4d12
Gartner24 <santiagovalencialeon@gmail.com>
2026-02-13 22:29:57 -0500

    Fix yarnking

--- lua/plugins/osc52.lua
+++ lua/plugins/osc52.lua
@@ -1,9 +1,13 @@
----OSC52 clipboard for VPS/SSH: copy from remote Neovim to local (Windows) clipboard.
+---OSC52 clipboard for VPS/SSH: copy from remote Neovim to local clipboard.
 ---Only loads when NOT in WSL; in WSL we use win32yank instead.
+---Requires tmux: set -g allow-passthrough on (for tmux 3.3a+)
 return {
 	"ojroques/nvim-osc52",
 	cond = not require("utils.env").is_wsl(),
 	config = function()
+		require("osc52").setup({
+			tmux_passthrough = true, -- required when using tmux with allow-passthrough on
+		})
 		local function copy(lines, _)
 			require("osc52").copy(table.concat(lines, "\n"))
 		end
