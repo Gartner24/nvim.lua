0821c6526280f00e77ce41fcce60dfdd4925ba45
Gartner24 <santiagovalencialeon@gmail.com>
2026-02-13 22:32:45 -0500

    Fix yarnking

--- lua/plugins/osc52.lua
+++ lua/plugins/osc52.lua
@@ -1,6 +1,7 @@
 ---OSC52 clipboard for VPS/SSH: copy from remote Neovim to local clipboard.
 ---Only loads when NOT in WSL; in WSL we use win32yank instead.
 ---Requires tmux: set -g allow-passthrough on (for tmux 3.3a+)
+---Paste from outside: use Ctrl+Shift+V in terminal (OSC52 paste not widely supported)
 return {
 	"ojroques/nvim-osc52",
 	cond = not require("utils.env").is_wsl(),
@@ -8,18 +9,15 @@ return {
 		require("osc52").setup({
 			tmux_passthrough = true, -- required when using tmux with allow-passthrough on
 		})
-		local function copy(lines, _)
-			require("osc52").copy(table.concat(lines, "\n"))
-		end
-		-- Paste: most terminals don't support OSC52 paste; use Ctrl+Shift+V in terminal
-		local function paste()
-			return { {}, "v" }
-		end
-		vim.g.clipboard = {
-			name = "osc52",
-			copy = { ["+"] = copy, ["*"] = copy },
-			paste = { ["+"] = paste, ["*"] = paste },
-		}
-		vim.opt.clipboard = "unnamedplus"
+		-- Copy every yank to OSC52 (more reliable than clipboard provider)
+		vim.api.nvim_create_autocmd("TextYankPost", {
+			group = vim.api.nvim_create_augroup("osc52_yank", { clear = true }),
+			callback = function()
+				require("osc52").copy_register('"')
+			end,
+		})
+		-- Don't set clipboard provider: it breaks p (paste returns empty).
+		-- Use unnamed register only; yank still sends to OSC52 via autocmd above.
+		vim.opt.clipboard = "unnamed"
 	end,
 }
